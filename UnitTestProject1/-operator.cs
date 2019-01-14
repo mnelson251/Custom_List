5c990a5d804f0d8a85169d794a7a7785ee81cc79
Matthew Nelson <mnelson251@gmail.com>
2019-01-14 10:54:16 -0600

    minus operator started

---
+++ UnitTestProject1/-operator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MinusOperator
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+        }
+
+
+
+
+    }
+}
